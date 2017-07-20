d382953ed381dd30ebf8112c2f737e98f73e0074
Zach Morse <zacharymorse@gmail.com>
2017-07-20 18:57:37 -0500

    styling and datahandling working

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,26 +3,29 @@ import React, { Component } from "react";
 class Form extends Component {
   render() {
     return (
-      <div className="card">
-        <h3>What is your name, pilot?</h3>
-        <form className="form" onSubmit={this.props.setPilot}>
-          <input
-            type="text"
-            placeholder="enter your name"
-            onKeyUp={this.props.inputAction}
-            name="TempPilot"
-          />
-          <br />
-          <input
-            type="submit"
-            value="Submit"
-            className="btn"
-            disabled={!this.props.tempPilot}
-          />
-          <h3>
-            {this.props.pilot}
-          </h3>
-        </form>
+      <div className="text-center">
+        <div className="card">
+          <h3>What is your name, pilot?</h3>
+          <form className="form" onSubmit={this.props.setPilot}>
+            <input
+              type="text"
+              placeholder="enter your name"
+              onKeyUp={this.props.inputAction}
+              name="TempPilot"
+            />
+            <br />
+            <br />
+            <input
+              type="submit"
+              value="Submit"
+              className="btn btn-primary"
+              disabled={!this.props.tempPilot}
+            />
+            <h3>
+              {this.props.pilot}
+            </h3>
+          </form>
+        </div>
       </div>
     );
   }
--- src/components/VehicleCards.js
+++ src/components/VehicleCards.js
@@ -4,20 +4,52 @@ class VehicleCards extends Component {
   render() {
     let vehicles = this.props.state.vehicles.map(e => {
       return (
-        <div key={e.name}>
-          <h1>
-            {e.name}
-          </h1>
+        <div className="col-sm-4">
+          <div key={e.name} className="card">
+            <div className="card-block">
+              <h5 className="card-title">
+                Vehicle: {e.name}
+              </h5>
+              <h6 className="card-subtitle text-muted">
+                Model: {e.model}
+              </h6>
+              <br />
+              <div className="card">
+                <div className="card-block">
+                  <h4 className="card-subtitle text-muted">Specs:</h4>
+                  <ul className="list-group list-group-flush">
+                    <li className="list-group-item">
+                      Manufacturer: {e.manufacturer}
+                    </li>
+                    <li className="list-group-item">
+                      Class: {e.vehicle_class}
+                    </li>
+                    <li className="list-group-item">
+                      Passengers: {e.passengers}
+                    </li>
+                    <li className="list-group-item">
+                      Crew: {e.crew}
+                    </li>
+                    <li className="list-group-item">
+                      Length: {e.length}
+                    </li>
+                    <li className="list-group-item">
+                      Max Speed: {e.max_atmosphering_speed}
+                    </li>
+                    <li className="list-group-item">
+                      Cargo Capacity: {e.cargo_capacity}
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       );
     });
     return (
-      <div className="card-deck">
-        <div className="card">
-          <h1>
-            {vehicles}
-          </h1>
-        </div>
+      <div className="row">
+        {vehicles}
       </div>
     );
   }
